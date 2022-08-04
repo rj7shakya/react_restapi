139302b0e7ff5c8b82baa847da6737767a9d148c
rj7shakya <shakyarajad1@gmail.com>
2022-08-04 19:19:36 +0545

    get delete code added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import UserItem from "./UserItem";
 
 function App() {
+  const [users, setUsers] = useState(null);
+
+  const deleteUser = (id) => {
+    console.log("id", id);
+    console.log("users", users);
+    console.log(
+      "filtered",
+      users.filter((i) => i.id !== id)
+    );
+    setUsers(users.filter((i) => i.id !== id));
+
+    // axios
+    // .delete(`https://fakerestapi123.herokuapp.com/users/${id}`)
+    // .then(() => {
+    //   // console.log("delete");
+
+    //     // users.filter(  id )
+    //   })
+    //   .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    const call = () => {
+      axios
+        .get("https://fakerestapi123.herokuapp.com/users")
+        .then((res) => {
+          setUsers(res?.data);
+        })
+        .catch((err) => console.log(err));
+    };
+
+    call();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users ? (
+        users.map((item, index) => (
+          <UserItem item={item} key={index} deleteUser={deleteUser} />
+        ))
+      ) : (
+        <div>loading</div>
+      )}
     </div>
   );
 }
